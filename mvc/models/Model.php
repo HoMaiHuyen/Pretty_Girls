d5be3a6b06be6f5b226778690033a30a822042c0
Linh <nguyenthilinhqni2020@gmail.com>
2023-12-12 22:41:40 +0700

    Config project

--- mvc/models/Model.php
+++ mvc/models/Model.php
@@ -1,15 +1,17 @@
 <?php 
-// require_once dirname(__DIR__)."/core/Connect.php";
+require_once dirname(__DIR__)."/core/DotEnvironment.php";
     class Model{
         protected $table;
         protected $connect;
 
         function __construct()
         {
-            $host     = 'localhost'; // Because MySQL is running on the same computer as the web server
-            $database = 'PHP_connect'; // Name of the database you use (you need first to CREATE DATABASE in MySQL)
-            $user     = 'root'; // Default username to connect to MySQL is root
-            $password = ''; // Default password to connect to MySQL is empty
+            $evn= new DotEnvironment();
+            $evn->load();
+            $host     = getenv('DB_HOST'); // Because MySQL is running on the same computer as the web server
+            $database = getenv('DB_NAME') ;// Name of the database you use (you need first to CREATE DATABASE in MySQL)
+            $user     = getenv('DB_USER'); // Default username to connect to MySQL is root
+            $password = getenv('DB_PASSWORD'); // Default password to connect to MySQL is empty
         
             try {
                 $this->connect = new PDO("mysql:host=$host;dbname=$database", $user, $password);
