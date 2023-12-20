e7ad7e5831d940c9ca99663ae629f687aa1258c4
sang.ho <sang.ho25@student.passerellesnumeriques.org>
2023-12-20 20:29:00 +0700

    fix send mail

--- mvc/controllers/MailController.php
+++ mvc/controllers/MailController.php
@@ -1,11 +1,13 @@
 <?php 
-require_once dirname(__DIR__).'/core/Services/MailService.php';
+require_once dirname(__DIR__).'/core/Services/Mail/WelcomeMailService.php';
 class MailController {
     public function index($params){
-     $mail= new MailService();
-     $mail->sendMail($params);
-       
+     $mail= new WelcomeMailService();
+     $mail->setBodyEmail();
+     $mail->sendMail([
+        'email'=>'sang.ho25@student.passerellesnumeriques.org',
+        'name'=>'Sang'
+        ]);  
     }
 }
-
 ?>
---
+++ mvc/core/Services/Mail/MailService.php
@@ -0,0 +1,37 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require_once  dirname(dirname(dirname(dirname(__DIR__)))) . '/vendor/autoload.php';
+
+abstract class MailService
+{
+    protected $bodyEmail;
+    public function sendMail($email)
+    {
+        $mail = new PHPMailer(true);
+        try {
+            $mail->isSMTP();
+            $mail->Host       = 'smtp.gmail.com';
+            $mail->SMTPAuth   = true;
+            $mail->Username   = getenv('MAIL_SHOP');
+            $mail->Password   = getenv('MAIL_PASSWORD');
+            $mail->SMTPDebug=0; 
+            $mail->SMTPSecure = 'tls';
+            $mail->Port       = 587;
+            $mail->setFrom(getenv('MAIL_SHOP'), 'Mailer');
+            $mail->addAddress($email['email'], $email['name']);
+            $mail->isHTML(true);
+            $mail->Subject = '[ FORM CONTACT from Blue Cosmetics ]';
+                       
+            $mail->Body    = $this->bodyEmail;
+            $mail->send();
+            echo 'Message has been sent';
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+    }
+   abstract public function setBodyEmail();
+
+   
+}
