753dbafb71c9d0925fa31981a0ede35e439cb034
sang.ho <sang.ho25@student.passerellesnumeriques.org>
2023-12-28 07:43:43 +0700

    fix (TN19-send-mail) : add body message

--- mvc/core/Services/Mail/WelcomeMailService.php
+++ mvc/core/Services/Mail/WelcomeMailService.php
@@ -5,7 +5,10 @@ class WelcomeMailService extends MailService{
     
     public function setBodyEmail()
     {
-        $this->bodyEmail= "Welcome to the system";
+        $this->bodyEmail = "<h3>Dear you!,</h3>\n";
+        $this->bodyEmail .= "<h3>Welcome to our system! We are excited to have you on board.</h3>\n";
+        $this->bodyEmail .= "<h4>Thank you for joining us!</h4>\n";
+        $this->bodyEmail .= "Best regards!,";
     }
     
 } 
