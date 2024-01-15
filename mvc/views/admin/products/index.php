25ad95e80db7afe6ec658e5125f16382b4a46790
Linh <nguyenthilinhqni2020@gmail.com>
2024-01-15 09:44:16 +0700

    fix-TN25-CRUDProduct:: add search Product

--- mvc/views/admin/products/index.php
+++ mvc/views/admin/products/index.php
@@ -4,7 +4,6 @@
      <main>
          <div class="container-fluid px-4">
              <h1 class="mt-4">Products</h1>
-
              <div class="card mb-4">
                  <div class="card-header">
                      <i class="fas fa-table me-1"></i>
